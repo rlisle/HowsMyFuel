93120cc72cccf3508178ee2e217903376ef01dd4
Ron Lisle <ron@lisles.net>
2013-03-25 18:25:06 -0500

    Add unit test target

---
+++ UnitTests/UnitTests.m
@@ -0,0 +1,32 @@
+//
+//  UnitTests.m
+//  UnitTests
+//
+//  Created by Ron Lisle on 3/25/13.
+//  Copyright (c) 2013 Ron Lisle. All rights reserved.
+//
+
+#import "UnitTests.h"
+
+@implementation UnitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in UnitTests");
+}
+
+@end
