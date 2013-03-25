531733d41a26ae9eb00741bb4a91aafee08cab8c
Ron Lisle <ron@lisles.net>
2013-03-25 18:14:11 -0500

    Initial Empty App

---
+++ HowsMyFuel/HMFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HMFAppDelegate.h
+//  HowsMyFuel
+//
+//  Created by Ron Lisle on 3/25/13.
+//  Copyright (c) 2013 Ron Lisle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HMFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HowsMyFuel/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HowsMyFuel
+//
+//  Created by Ron Lisle on 3/25/13.
+//  Copyright (c) 2013 Ron Lisle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HMFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HMFAppDelegate class]));
+    }
+}
